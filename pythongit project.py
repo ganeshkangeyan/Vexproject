5a4e5d650c6245beaad6bfbfacb6547c834e0645
ganeshkangeyan <ganeshk@redantlabs.io>
2025-09-18 21:04:57 +0530

    add new from pythongit

--- pythongit project.py
+++ pythongit project.py
@@ -2,8 +2,9 @@ a= 10
 b= 20
 c="hello"
 print(a,b)
-ad= ("{}".format(a)+c)
-print(ad)
+ac= ("{}".format(a)+c)
+print(ac)
+# add new from pythongit
 bc=["hello",12,343,6565,"testing","dddd","oooo","qqqq"]
 bc[1]="jjjjj"
 print(bc)
