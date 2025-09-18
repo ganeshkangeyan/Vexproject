115f141e397199d517ba1b878c200751d1d2770d
ganeshkangeyan <ganeshk@redantlabs.io>
2025-09-18 14:41:56 +0530

    new file from git project cloun No 1

---
+++ pythongit project.py
@@ -0,0 +1,13 @@
+a= 10
+b= 20
+c="hello"
+print(a,b)
+ad= ("{}".format(a)+c)
+print(ad)
+bc=["hello",12,343,6565,"testing","dddd","oooo","qqqq"]
+bc[1]="jjjjj"
+print(bc)
+bc.append("uuuuuu")
+print(bc)
+bc.insert(6,c)
+print(bc)
